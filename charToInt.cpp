f77e31cabbe3eb78ba5115e689f9e92eb388bbdd
Matheus Rodrigues Fernandes <matheusrf96@gmail.com>
2017-06-10 21:53:03 -0300

    Create charToInt.cpp

--- charToInt.cpp
+++ charToInt.cpp
@@ -6,15 +6,15 @@ int main(){
 	string maiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	string minusculas = "abcdefghijklmnopqrstuvwxyz";
 
-    cout << "MAIUSCULAS: " << endl;
+    	cout << "MAIUSCULAS: " << endl;
 
 	for(int i = 0; i < maiusculas.length(); i++){
-        cout << maiusculas[i] << ": " << (int)maiusculas[i] << endl;
+        	cout << maiusculas[i] << ": " << (int)maiusculas[i] << endl;
 	}
 
 	cout << endl << "minusculas: " << endl;
 
 	for(int i = 0; i < minusculas.length(); i++){
-        cout << minusculas[i] << ": " << (int)minusculas[i] << endl;
+        	cout << minusculas[i] << ": " << (int)minusculas[i] << endl;
 	}
 }
